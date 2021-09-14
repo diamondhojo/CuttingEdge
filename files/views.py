988262641247231f91a2375cc93b22e4cf4f5a42
Jordan Race <diamondhojo@gmail.com>
2021-09-13 20:21:13 -0400

    Initial
    Working base of website

---
+++ files/views.py
@@ -0,0 +1,13 @@
+
+from flask import Blueprint, render_template
+
+views = Blueprint("views", __name__)
+
+@views.route("/home")
+@views.route("/")
+def home():
+    return render_template("home.html")
+
+@views.route("/profile")
+def profile():
+    return "Profile"
