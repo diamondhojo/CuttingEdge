3082d53523ff4f7905cab7d817682504afdd885b
Jordan Race <diamondhojo@gmail.com>
2021-09-16 17:10:08 -0400

    Login/Logout System Fully Functional
    -Functional authentication system
    -Functional database creation and use
    -Nav bar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+  
 from files import create_app
 
 if __name__ == "__main__":
--- files/views.py
+++ files/views.py
@@ -1,16 +1,18 @@
 
 from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, current_user
+from .models import User
 
 views = Blueprint("views", __name__)
 
-@views.route("/")
-def home_1():
-    return redirect(url_for("views.home"))
-
 @views.route("/home")
+@login_required
 def home():
-    return render_template("home.html")
+    users = User.query.all()
+    return render_template("home.html", user=current_user, users=users)
 
+"""
 @views.route("/profile")
 def profile():
     return render_template("profile.html")
+"""
