4fba4adf1a46edaf9dd2b5c4930c91da17aab549
Jordan Race <diamondhojo@gmail.com>
2021-10-19 21:34:06 -0400

    Update __init__.py

--- files/__init__.py
+++ files/__init__.py
@@ -35,16 +35,16 @@ def create_app():
     return app
 
 def create_database(app):
+    password = generate_password_hash("password", method='sha256')
     if not path.exists("files/" + DB_NAME):
         db.create_all(app=app)
         # this gets all the column names
         conn = sqlite3.connect("files/database.db")
         cur = conn.cursor()
-        password = generate_password_hash("password", method='sha256')
 
         #dummy data when creating new database (to be able to log in under new environment)
-        cur.execute('INSERT INTO client(id, first_name, last_Name, email, address, date_created, date_updated) VALUES (1, \'Casey\', \'Damude\', \'casey.damude0232@hotmail.com\', \'8 Hamilton\', \'2021-10-04 18:38:54.501512\', \'2021-10-04 18:38:54.501512\')')
-        cur.execute("INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (1, 'Jordan', 'Race', 'diamondhojo@gmail.com', '4 Swan', password, 'Employee', '2021-10-04 18:38:54.501512', '2021-10-04 18:38:54.501512'))
+        cur.execute('INSERT INTO client(id, first_name, last_Name, email, address, date_created, date_updated) VALUES (1, \'John\', \'Appleseed\', \'jappleseed@gmail.com\', \'1795 Brentwood Drive\', \'2021-10-04 18:38:54.501512\', \'2021-10-04 18:38:54.501512\')')
+        cur.execute("INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (1, 'Lorem', 'Ipsum', 'lipsum@gmail.com', '139 Young Road', password, 'Manager', '2021-10-04 18:38:54.501512', '2021-10-04 18:38:54.501512'))
 
         #Client column names
         cur.execute("SELECT * FROM client")
@@ -71,13 +71,13 @@ def create_database(app):
             
             if result == '(0,)':
                 print("\nNo clients found, inserting dummy data\n")
-                cur.execute('INSERT INTO client(id, first_name, last_Name, email, address, date_created, date_updated) VALUES (1, \'Dummy\', \'Data\', \'DummyEmail@gmail.com\', \'DummyAddress\', \'2021-10-04 18:38:54.501512\', \'2021-10-04 18:38:54.501512\')')
+                cur.execute('INSERT INTO client(id, first_name, last_Name, email, address, date_created, date_updated) VALUES (1, \'John\', \'Appleseed\', \'jappleseed@gmail.com\', \'1795 Brentwood Drive\', \'2021-10-04 18:38:54.501512\', \'2021-10-04 18:38:54.501512\')')
             
             conn.commit()
         except:
             print("\nNo table such as 'client' exists. Creating table and inserting dummy data\n")
             cur.execute('CREATE TABLE client(id INT, first_name VARCHAR(50), last_name VARCHAR(50), email VARCHAR(100), address VARCHAR(100), date_created DATETIME, date_updated DATETIME);')
-            cur.execute('INSERT INTO client(id, first_name, last_Name, email, address, date_created, date_updated) VALUES (1, \'Dummy\', \'Data\', \'DummyEmail@gmail.com\', \'DummyAddress\', \'2021-10-04 18:38:54.501512\', \'2021-10-04 18:38:54.501512\')')
+            cur.execute('INSERT INTO client(id, first_name, last_Name, email, address, date_created, date_updated) VALUES (1, \'John\', \'Appleseed\', \'jappleseed@gmail.com\', \'1795 Brentwood Drive\', \'2021-10-04 18:38:54.501512\', \'2021-10-04 18:38:54.501512\')')
             
             cur.execute("SELECT * FROM client")
             views.clientCols = [tuple[0] for tuple in cur.description]
@@ -90,14 +90,14 @@ def create_database(app):
 
             if cur.execute("SELECT COUNT(*) FROM employee") == 0:
                 print("\nNo employees found, inserting dummy data\n")
-                cur.execute('INSERT INTO employee(id, first_name, last_Name, email, address, password, position, date_created, date_updated) VALUES (1, \'DummyName\', \'DummyLastName\', \'Dummy@gmail.com\', \'Dummy\', \'Dummy\', password, \'Employee\', \'2021-10-04 18:38:54.501512\', \'2021-10-04 18:38:54.501512\')')
+                cur.execute("INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (1, 'Lorem', 'Ipsum', 'lipsum@gmail.com', '139 Young Road', password, 'Manager', '2021-10-04 18:38:54.501512', '2021-10-04 18:38:54.501512'))
 
             conn.commit()
         except:
             print("\nNo table such as 'employee' exists. Creating table and inserting dummy data\n")
             
             cur.execute('CREATE TABLE employee(id INT, first_name VARCHAR(50), last_name VARCHAR(50), email VARCHAR(100), address VARCHAR(100), password VARCHAR(100), position VARCHAR(15), date_created DATETIME, date_updated DATETIME);')
-            cur.execute('INSERT INTO employee(id, first_name, last_Name, email, address, password, position, date_created, date_updated) VALUES (1, \'Jordan\', \'Race\', \'diamondhojo@gmail.com\', \'PrisnMike\', \'4 Swan\', password, \'Employee\', \'2021-10-04 18:38:54.501512\', \'2021-10-04 18:38:54.501512\')')
+            cur.execute("INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (1, 'Lorem', 'Ipsum', 'lipsum@gmail.com', '139 Young Road', password, 'Manager', '2021-10-04 18:38:54.501512', '2021-10-04 18:38:54.501512'))
             
             cur.execute("SELECT * FROM employee")
             views.empCols = [tuple[0] for tuple in cur.description]
